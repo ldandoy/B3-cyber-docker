3469a3447d7cab43bf986d4e926bcadc27bce0d3
ldandoy <ldandoy@overconsulting.net>
2023-02-13 15:59:38 +0100

    MAJ

--- app/index.php
+++ app/index.php
@@ -2,4 +2,36 @@
 
 echo "Hello World !";
 
+$dsn = 'mysql:dbname=b3cyber;host=mysql';
+$user = 'root';
+$password = '';
+
+$link = new PDO($dsn, $user, $password);
+
+/*$rows = $link->exec("SELECT * FROM tasks");
+
+var_dump($rows);*/
+
+$sql = 'SELECT * FROM tasks WHERE id = ' . $_GET['id'];
+
 ?>
+<table>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Label</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($link->query($sql) as $row) { ?>
+            <tr>
+                <td>
+                    <?php echo $row['id']; ?>
+                </td>
+                <td>
+                    <?php echo $row['label']; ?>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
